a3e88844625369d3302fe2422a08fb997fdd080b
WahyuSeptiadi <arstuw@gmail.com>
2020-04-21 13:23:13 +0700

    nyoba btnBack wahyu

--- app/src/main/java/com/newbie/handycraftsshop/BuyActivity.java
+++ app/src/main/java/com/newbie/handycraftsshop/BuyActivity.java
@@ -2,13 +2,28 @@ package com.newbie.handycraftsshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class BuyActivity extends AppCompatActivity {
+
+    private ImageView btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy);
+
+        btnBack = findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toHome = new Intent(BuyActivity.this, HomeActivity.class);
+                startActivity(toHome);
+            }
+        });
     }
 }
--- app/src/main/java/com/newbie/handycraftsshop/HomeActivity.java
+++ app/src/main/java/com/newbie/handycraftsshop/HomeActivity.java
@@ -2,13 +2,28 @@ package com.newbie.handycraftsshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Button btnBuy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        btnBuy = findViewById(R.id.btnBuy);
+
+        btnBuy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toBuy = new Intent(HomeActivity.this, BuyActivity.class);
+                startActivity(toBuy);
+            }
+        });
     }
 }
