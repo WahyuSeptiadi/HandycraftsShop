2d906f2872abc155c39df1f9190b80f1c3c7d811
WahyuSeptiadi <arstuw@gmail.com>
2020-04-26 15:22:35 +0700

    update error register

--- app/src/main/java/com/newbie/handycraftsshop/Activity/DashbordActivity.java
+++ app/src/main/java/com/newbie/handycraftsshop/Activity/DashbordActivity.java
@@ -11,22 +11,13 @@ import com.newbie.handycraftsshop.R;
 
 public class DashbordActivity extends AppCompatActivity {
 
-    private Button login, register;
+    private Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashbord);
 
         login = findViewById(R.id.btn_login_dashbord);
-        register = findViewById(R.id.btn_register_dashbord);
-
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent toRegister = new Intent(DashbordActivity.this, RegisterActivity.class);
-                startActivity(toRegister);
-            }
-        });
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
