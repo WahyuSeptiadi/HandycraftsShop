db8860a62341d81420b8c5cd6e37ee819150feb6
alfisyar520 <alfisyarpranata@gmail.com>
2020-04-27 12:59:57 +0700

    tambah logout

--- app/src/main/java/com/newbie/handycraftsshop/Activity/ProfileActivity.java
+++ app/src/main/java/com/newbie/handycraftsshop/Activity/ProfileActivity.java
@@ -1,12 +1,17 @@
 package com.newbie.handycraftsshop.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.newbie.handycraftsshop.R;
 
 public class ProfileActivity extends AppCompatActivity {
@@ -15,5 +20,27 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return  true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logout:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(ProfileActivity.this, LoginActivity.class));
+                finish();
+                return true;
+        }
+        return false;
     }
 }
