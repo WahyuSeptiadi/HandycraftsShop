3e859b9e62f8fb1986328e20043045347c509025
WahyuSeptiadi <arstuw@gmail.com>
2020-05-05 10:00:49 +0700

    edit profile

---
+++ app/src/main/java/com/newbie/handycraftsshop/Adapter/ListPostProfileAdapter.java
@@ -0,0 +1,9 @@
+package com.newbie.handycraftsshop.Adapter;
+
+/**
+ * Created by wahyu_septiadi on 05, May 2020.
+ * Visit My GitHub --> https://github.com/WahyuSeptiadi
+ */
+
+public class ListPostProfileAdapter {
+}
